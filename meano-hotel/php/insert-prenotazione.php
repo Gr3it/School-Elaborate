e61426f0896f5ab68eb35287599f7ccc3bb39969
Emanuele.Zini <emanuele.zini11@gmail.com>
2021-05-29 08:48:46 +0200

    fixed insert.php

--- meano-hotel/php/insert-prenotazione.php
+++ meano-hotel/php/insert-prenotazione.php
@@ -24,41 +24,37 @@ $result = $stmt->get_result();
 $stmt->close();
 
 if ($result->num_rows > 0) {
-  $query = "";
-  $types = "";
+
 }
 else {
   $query = "INSERT INTO cliente(Codice_Fiscale, Nome, Cognome, Email, Data_Di_Nascita) VALUES (?,?,?,?,?); ";
-  $types = "sssss";
-  $params[] = $codice_fiscale;
-  $params[] = $nome;
-  $params[] = $cognome;
-  $params[] = $email;
-  $params[] = $data_di_nascita;
+
+  $stmt = $conn->prepare($query);
+
+  $stmt->bind_param("sssss", $codice_fiscale, $nome, $cognome, $email, $data_di_nascita);
+  $stmt->execute();
+
+  $stmt->close();
 }
 
-$query = $query."INSERT INTO prenotazione(Codice_Fiscale_Cliente, Data_Inizio_Prenotazione, Data_Fine_Prenotazione, Pagamento_Caparra) VALUES (?,?,?,?); ";
-$types = $types."sssi";
-$params[] = $codice_fiscale;
-$params[] = $inizio;
-$params[] = $fine;
-$params[] = 0;
-
-foreach($stanze as $stanza){
-  $query = $query."INSERT INTO prenotazione_comprende_camera(Nome_Camera, Id_Prenotazione) VALUES (?,LAST_INSERT_ID);";
-  $types = $types."s";
-  $params[] = $stanza;
-}
-
+$query = "INSERT INTO prenotazione(Codice_Fiscale_Cliente, Data_Inizio_Prenotazione, Data_Fine_Prenotazione, Pagamento_Caparra) VALUES (?,?,?,?); ";
+$var = 0;
 $stmt = $conn->prepare($query);
 
-/* file_put_contents("./debug.txt",$query.print_r($params,true)); */
-file_put_contents("./debug.txt",print_r($stmt,true),FILE_APPEND);
-file_put_contents("./debug.txt",$stmt,FILE_APPEND);
-
-/* $stmt->bind_param($types, ...$params);
+$stmt->bind_param("sssi", $codice_fiscale, $inizio, $fine, $var);
 $stmt->execute();
-$result = $stmt->get_result();
 
-$stmt->close(); */
+$stmt->close();
+
+$last_insert = $conn->insert_id;
+
+foreach($stanze as $stanza){
+  $query = "INSERT INTO prenotazione_comprende_camera(Nome_Camera, Id_Prenotazione) VALUES (?,?);";
+  $stmt = $conn->prepare($query);
+  $stmt->bind_param("si", $stanza, $last_insert);
+  $stmt->execute();
+
+  $stmt->close();
+}
+
 $conn->close();
