8af34d8896676efa2a7696fdc3cb8b3e5d9dc658
Gr3it <xxgreitxx@gmail.com>
2021-06-21 21:47:49 +0200

    update

--- meano-hotel/js/formjs.js
+++ meano-hotel/js/formjs.js
@@ -1,30 +1,46 @@
-document.querySelector(".avanti1").addEventListener("click", function () {
-  var element = document.querySelector(".formh1");
-  element.classList.add("deactive");
-  var element = document.querySelector(".formh2");
-  element.classList.remove("deactive");
-  var element = document.querySelector(".formb1");
-  element.classList.add("deactive");
-  var element = document.querySelector(".formb2");
-  element.classList.remove("deactive");
-
-  inizio = document.getElementById("arrivo").value;
-  fine = document.getElementById("partenza").value;
-
-  $.ajax({
-    url: "./php/select-stanze-occupate.php",
-    type: "POST",
-    data: {
-      inizio: inizio,
-      fine: fine,
-    },
-    success: function (data) {
-      JSON.parse(data).forEach((element) => {
-        var element = document.querySelector("." + element);
-        element.classList.add("occupied");
-      });
-    },
+document
+  .querySelector(".pop-up-container")
+  .addEventListener("click", function () {
+    var element = document.querySelector(".pop-up-container");
+    element.classList.remove("pop-up-active");
   });
+
+document.querySelector(".avanti1").addEventListener("click", function () {
+  if (
+    document.getElementById("arrivo").value == "" ||
+    document.getElementById("partenza").value == ""
+  ) {
+    var element = document.querySelector(".pop-up-container");
+    element.classList.add("pop-up-active");
+  } else {
+    console.log(document.getElementById("arrivo").value);
+    var element = document.querySelector(".formh1");
+    element.classList.add("deactive");
+    var element = document.querySelector(".formh2");
+    element.classList.remove("deactive");
+    var element = document.querySelector(".formb1");
+    element.classList.add("deactive");
+    var element = document.querySelector(".formb2");
+    element.classList.remove("deactive");
+
+    inizio = document.getElementById("arrivo").value;
+    fine = document.getElementById("partenza").value;
+
+    $.ajax({
+      url: "./php/select-stanze-occupate.php",
+      type: "POST",
+      data: {
+        inizio: inizio,
+        fine: fine,
+      },
+      success: function (data) {
+        JSON.parse(data).forEach((element) => {
+          var element = document.querySelector("." + element);
+          element.classList.add("occupied");
+        });
+      },
+    });
+  }
 });
 
 document.querySelector(".indietro2").addEventListener("click", function () {
@@ -39,14 +55,19 @@ document.querySelector(".indietro2").addEventListener("click", function () {
 });
 
 document.querySelector(".avanti2").addEventListener("click", function () {
-  var element = document.querySelector(".formh2");
-  element.classList.add("deactive");
-  var element = document.querySelector(".formh3");
-  element.classList.remove("deactive");
-  var element = document.querySelector(".formb2");
-  element.classList.add("deactive");
-  var element = document.querySelector(".formb3");
-  element.classList.remove("deactive");
+  if (document.querySelectorAll(".selected").length == 0) {
+    var element = document.querySelector(".pop-up-container");
+    element.classList.add("pop-up-active");
+  } else {
+    var element = document.querySelector(".formh2");
+    element.classList.add("deactive");
+    var element = document.querySelector(".formh3");
+    element.classList.remove("deactive");
+    var element = document.querySelector(".formb2");
+    element.classList.add("deactive");
+    var element = document.querySelector(".formb3");
+    element.classList.remove("deactive");
+  }
 });
 
 document.querySelector(".indietro3").addEventListener("click", function () {
@@ -61,98 +82,109 @@ document.querySelector(".indietro3").addEventListener("click", function () {
 });
 
 document.querySelector(".avanti3").addEventListener("click", function () {
-  var lunghezza = 999;
-  var element = document.querySelector(".formh3");
-  element.classList.add("deactive");
-  var element = document.querySelector(".formh4");
-  element.classList.remove("deactive");
-  var element = document.querySelector(".formb3");
-  element.classList.add("deactive");
-  var element = document.querySelector(".formb4");
-  element.classList.remove("deactive");
+  if (
+    $("#nome").val() == "" ||
+    $("#cognome").val() == "" ||
+    $("#codice_fiscale").val() == "" ||
+    $("#email").val() == "" ||
+    $("#data_di_nascita").val() == ""
+  ) {
+    var element = document.querySelector(".pop-up-container");
+    element.classList.add("pop-up-active");
+  } else {
+    var lunghezza = 999;
+    var element = document.querySelector(".formh3");
+    element.classList.add("deactive");
+    var element = document.querySelector(".formh4");
+    element.classList.remove("deactive");
+    var element = document.querySelector(".formb3");
+    element.classList.add("deactive");
+    var element = document.querySelector(".formb4");
+    element.classList.remove("deactive");
 
-  let firstDate = $("#arrivo").val();
-  let secondDate = $("#partenza").val();
-  const findTheDifferenceBetweenTwoDates = (firstDate, secondDate) => {
-    firstDate = new Date(firstDate);
-    secondDate = new Date(secondDate);
+    let firstDate = $("#arrivo").val();
+    let secondDate = $("#partenza").val();
+    const findTheDifferenceBetweenTwoDates = (firstDate, secondDate) => {
+      firstDate = new Date(firstDate);
+      secondDate = new Date(secondDate);
 
-    let timeDifference = Math.abs(secondDate.getTime() - firstDate.getTime());
+      let timeDifference = Math.abs(secondDate.getTime() - firstDate.getTime());
 
-    let millisecondsInADay = 1000 * 3600 * 24;
+      let millisecondsInADay = 1000 * 3600 * 24;
 
-    let differenceOfDays = Math.ceil(timeDifference / millisecondsInADay);
+      let differenceOfDays = Math.ceil(timeDifference / millisecondsInADay);
 
-    return differenceOfDays;
-  };
-  camera = document.querySelectorAll(".selected");
-  lunghezza = document.querySelectorAll(".selected").length;
+      return differenceOfDays;
+    };
+    camera = document.querySelectorAll(".selected");
+    lunghezza = document.querySelectorAll(".selected").length;
 
-  costo = 0;
-  persone = 0;
-  giorni = findTheDifferenceBetweenTwoDates(firstDate, secondDate);
+    costo = 0;
+    persone = 0;
+    giorni = findTheDifferenceBetweenTwoDates(firstDate, secondDate);
 
-  element = document.querySelector(".table");
-  element.innerHTML =
-    "<tr><th>Spesa</th><th>Numero Persone</th><th>Costo singolo giornaliero</th><th>Costo Totale</th></tr>";
+    element = document.querySelector(".table");
+    element.innerHTML =
+      "<tr><th>Spesa</th><th>Numero Persone</th><th>Costo singolo giornaliero</th><th>Costo Totale</th></tr>";
 
-  camera.forEach((element) => {
-    const element_name = element.classList[1];
-    $.ajax({
-      url: "./php/select-costo.php",
-      type: "POST",
-      data: {
-        camera: element.classList[1],
-      },
+    camera.forEach((element) => {
+      const element_name = element.classList[1];
+      $.ajax({
+        url: "./php/select-costo.php",
+        type: "POST",
+        data: {
+          camera: element.classList[1],
+        },
 
-      success: function (data) {
-        data = JSON.parse(data);
-        element = document.querySelector(".table");
-        costo_camera =
-          data[0]["Costo_A_Notte"] * data[0]["Numero_Posti"] * giorni;
-        persone += data[0]["Numero_Posti"];
-        costo += costo_camera;
-
-        element.innerHTML +=
-          "<tr><td>" +
-          [
-            element_name[0].toUpperCase(),
-            element_name.slice(1, 5),
-            " n°",
-            element_name.slice(5),
-          ].join("") +
-          "</td><td>" +
-          data[0]["Numero_Posti"] +
-          "</td><td><span>€ </span>" +
-          data[0]["Costo_A_Notte"] +
-          "</td><td><span>€ </span>" +
-          costo_camera +
-          "</td></tr>";
-
-        if (lunghezza == 1) {
+        success: function (data) {
+          data = JSON.parse(data);
           element = document.querySelector(".table");
+          costo_camera =
+            data[0]["Costo_A_Notte"] * data[0]["Numero_Posti"] * giorni;
+          persone += data[0]["Numero_Posti"];
+          costo += costo_camera;
+
           element.innerHTML +=
-            "<tr id='total-row'><td><b>Totale</b></td><td></td><td></td><td><span>€ </span>" +
-            costo +
-            "</td></tr>";
-          costo_caparra = costo + persone * 1.5 * giorni;
-          element.innerHTML +=
-            "<tr><td>Tassa di soggiorno</td><td>" +
-            persone +
+            "<tr><td>" +
+            [
+              element_name[0].toUpperCase(),
+              element_name.slice(1, 5),
+              " n°",
+              element_name.slice(5),
+            ].join("") +
+            "</td><td>" +
+            data[0]["Numero_Posti"] +
             "</td><td><span>€ </span>" +
-            1.5 +
+            data[0]["Costo_A_Notte"] +
             "</td><td><span>€ </span>" +
-            persone * 1.5 * giorni +
+            costo_camera +
             "</td></tr>";
-          element.innerHTML +=
-            "<tr id='total-row'><td><b>Totale con tassa di soggiorno</b></td><td></td><td></td><td><span>€ </span>" +
-            costo_caparra +
-            "</td></tr>";
-        }
-        lunghezza--;
-      },
+
+          if (lunghezza == 1) {
+            element = document.querySelector(".table");
+            element.innerHTML +=
+              "<tr id='total-row'><td><b>Totale</b></td><td></td><td></td><td><span>€ </span>" +
+              costo +
+              "</td></tr>";
+            costo_caparra = costo + persone * 1.5 * giorni;
+            element.innerHTML +=
+              "<tr><td>Tassa di soggiorno</td><td>" +
+              persone +
+              "</td><td><span>€ </span>" +
+              1.5 +
+              "</td><td><span>€ </span>" +
+              persone * 1.5 * giorni +
+              "</td></tr>";
+            element.innerHTML +=
+              "<tr id='total-row'><td><b>Totale con tassa di soggiorno</b></td><td></td><td></td><td><span>€ </span>" +
+              costo_caparra +
+              "</td></tr>";
+          }
+          lunghezza--;
+        },
+      });
     });
-  });
+  }
 });
 
 document.querySelector(".indietro4").addEventListener("click", function () {
